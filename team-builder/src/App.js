c5b4b925140cea6c74ed6344d936a5ea9b4bc132
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-13 23:11:02 -0400

    created add button and functionality

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 
 function App() {
   const [teamMember, setTeam] = useState([
-    { name: "Jackie", email: "abcd@gmail.com", role: "Full-Stack Dev" },
-    { name: "Polycarp", email: "1234@gmail.com", role: "Teacher" },
-    { name: "Edan", email: "milk@gmail.com", role: "Toddler" }
+    // { name: "Jackie", email: "abcd@gmail.com", role: "Full-Stack Dev" },
+    // { name: "Polycarp", email: "1234@gmail.com", role: "Teacher" },
+    // { name: "Edan", email: "milk@gmail.com", role: "Toddler" }
   ]);
   return (
     <div className="App">
-      <Form />
+      <Form setTeam={setTeam} />
       {teamMember.map(teamMember => (
         <Panels teamMember={teamMember} />
       ))}
